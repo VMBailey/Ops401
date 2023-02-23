c62ac65e71aa3a1817b18a37e2e23cf62e9ffa68
Vincent Bailey <75095846+VMBailey@users.noreply.github.com>
2023-02-23 15:56:29 -0600

    This is a script that creates a TCP client.

---
+++ TCPclient.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+# Script Name:      TCP Client
+# Author:           Vincent Bailey
+# Last Rev:         02/23/2023
+# Purpose:          This script will create a TCP client.
+# Source:           https://youtu.be/ugYfJNTawks
+
+##############################################################################
+# Libraries
+##############################################################################
+import socket
+
+##############################################################################
+# Variables
+##############################################################################
+clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host = socket.gethostname()
+port = 444
+
+##############################################################################
+# Main Function
+##############################################################################
+clientsocket.connect((host, port))
+
+# the code line below specifies how much data will be received throught the port.
+message = clientsocket.recv(1024)
+
+clientsocket.close()
+
+print(message.decode('ascii'))
